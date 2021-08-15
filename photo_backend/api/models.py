d063b0ab325236a5b7b8ca588f87d125c2423185
Jakub Kropáček <kropikuba@gmail.com>
2021-08-15 22:21:50 +0200

    Added feedback endpoints and fixed migrations?

--- photo_backend/api/models.py
+++ photo_backend/api/models.py
@@ -4,7 +4,7 @@ from photo_backend.extensions import db
 class Action(db.Model):
     __tablename__ = "action"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True, nullable=False)
+    name = db.Column(db.String(64), nullable=False)
     images = db.relationship("Image",
                              backref="action",
                              cascade="delete")
@@ -23,3 +23,14 @@ class Image(db.Model):
 
     def to_dict(self):
         return {"id": self.id, "link": self.link, "description": self.description}
+
+
+class Feedback(db.Model):
+    __tablename__ = "feedback"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, nullable=False)
+    subject = db.Column(db.String(64), nullable=False)
+    message = db.Column(db.String(512), nullable=False)
+
+    def to_dict(self):
+        return {"id": self.id, "email": self.email, "subject": self.subject, "message": self.message}
